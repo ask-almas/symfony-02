80a1ac5f5b91dde1c70fcd546adadab2e0e0ef83
Almas <ask.almas@gmail.com>
2018-10-31 16:23:15 +0600

    twig configurations

--- symfony-02/src/Twig/AppExtension.php
+++ symfony-02/src/Twig/AppExtension.php
@@ -11,11 +11,27 @@ namespace App\Twig;
 
 use Symfony\Bundle\TwigBundle\DependencyInjection\TwigExtension;
 use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
 
-class AppExtension extends AbstractExtension{
-    public function getFilters()
-    {
+class AppExtension extends AbstractExtension implements GlobalsInterface {
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    public function __construct(string $locale){
+        $this->locale = $locale;
+    }
+
+    public function getGlobals(){
+        return [
+            'locale' => $this->locale
+        ];
+    }
+
+    public function getFilters(){
        return [
            new TwigFilter('price', [$this, 'priceFilter'])
        ];
