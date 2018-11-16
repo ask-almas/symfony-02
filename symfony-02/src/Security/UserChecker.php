47329c76dc245caeafce04a509d31a5441af3438
ask-almas <ask.almas@gmail.com>
2018-11-16 10:34:00 +0600

    update 13

--- symfony-02/src/Security/UserChecker.php
+++ symfony-02/src/Security/UserChecker.php
@@ -8,31 +8,34 @@
 
 namespace App\Security;
 
-use App\Entity\User as AppUser;
-use Symfony\Component\Security\Core\Exception\AccountExpiredException;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\AccountStatusException;
+use Symfony\Component\Security\Core\Exception\DisabledException;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
-    public function checkPreAuth(UserInterface $user)
-    {
-        if (!$user instanceof AppUser) {
+
+    /**
+     * Checks the user account before authentication.
+     * @throws AccountStatusException
+     */
+    public function checkPreAuth(UserInterface $user){
+        if (!$user instanceof User) {
             return;
         }
-        if(!$user->getEnabled()){
-            return;
+
+        if (!$user->getEnabled()) {
+            throw new DisabledException();
         }
     }
 
-    public function checkPostAuth(UserInterface $user)
-    {
-        if (!$user instanceof AppUser) {
-            return;
-        }
+    /**
+     * Checks the user account after authentication.
+     * @throws AccountStatusException
+     */
+    public function checkPostAuth(UserInterface $user){
 
-        if(!$user->getEnabled()){
-            return;
-        }
     }
 }
